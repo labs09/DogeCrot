ab811690a684543f0d137fe1ad5e55e107d2123e
labs09 <30799220+labs09@users.noreply.github.com>
2019-05-27 10:20:17 +0800

    Update main.py

--- .core/main.py
+++ .core/main.py
@@ -161,9 +161,7 @@ UravxBuCwNMpYWTzKhPH else "("+me.username+")"))
        if UravxBuCwNMpYWTzKhcy==UravxBuCwNMpYWTzKhcD:
         UravxBuCwNMpYWTzKhcg('Skiping Ads ...',UravxBuCwNMpYWTzKhPE)
         UravxBuCwNMpYWTzKhci=UravxBuCwNMpYWTzKhco[-7:]
-        
-        UravxBuCwNMpYWTzKhcj=bot.send_message(chat_id=chat_id, text="Skip", reply_markup=reply_markup)
-
+        UravxBuCwNMpYWTzKhcj=markup.rows[0].buttons[1]
         UravxBuCwNMpYWTzKhcE=botcallback('Dogecoin_click_bot',UravxBuCwNMpYWTzKhcb.message.id,data=UravxBuCwNMpYWTzKhcj.encode())
         await UravxBuCwNMpYWTzKhcE
         break
